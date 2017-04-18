96079f79cb767574383b3f995ab45ce6d90c4f44
lx <870433758@qq.com>
2017-04-18 15:51:35 +0800

    no message

---
+++ public/server.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/4/17
+ * Time: 15:09
+ */
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new swoole_websocket_server("0.0.0.0", 9510);
+
+//监听WebSocket连接打开事件
+$ws->on('open', function ($ws, $request) {
+    $fd[] = $request->fd;
+    $GLOBALS['fd'][] = $fd;
+    //$ws->push($request->fd, "hello, welcome\n");
+});
+
+//监听WebSocket消息事件
+$ws->on('message', function ($ws, $frame) {
+    $msg =  "用户$frame->fd :$frame->data\n";
+    $msg =  json_encode($frame);
+//var_dump($GLOBALS['fd']);
+//exit;
+    foreach($GLOBALS['fd'] as $aa){
+        foreach($aa as $i){
+            $ws->push($i,$msg);
+        }
+    }
+    // $ws->push($frame->fd, "server: {$frame->data}");
+    // $ws->push($frame->fd, "server: {$frame->data}");
+});
+
+//监听WebSocket连接关闭事件
+$ws->on('close', function ($ws, $fd) {
+    echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
