456f90e0100fd9afbf6665a529362283ab0fead7
lx <870433758@qq.com>
2017-04-21 17:06:02 +0800

    no message

--- resources/views/Client/index.blade.php
+++ resources/views/Client/index.blade.php
@@ -1,50 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+<meta charset="utf-8">
+<title>聊天室 - editor:yinq</title>
+<link rel="shortcut icon" href="favicon.png">
+<link rel="icon" href="favicon.png" type="image/x-icon">
+<link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}">
+<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
 </head>
+
 <body>
-<div id="msg"></div>
-<input type="text" id="text">
-<input type="submit" value="发送数据" onclick="song()">
-</body>
-<script>
-    var msg = document.getElementById("msg");
-    var wsServer = 'ws://106.14.10.215:9505';
-    var websocket = new WebSocket(wsServer);
-    //onopen监听连接打开
-    websocket.onopen = function (evt) {
-        //websocket.readyState 属性：
-        /*
-         CONNECTING    0    The connection is not yet open.
-         OPEN    1    The connection is open and ready to communicate.
-         CLOSING    2    The connection is in the process of closing.
-         CLOSED    3    The connection is closed or couldn't be opened.
-         */
-        msg.innerHTML = websocket.readyState;
-    };
+<div class="chatbox">
+  <div class="chat_top fn-clear">
+    <div class="logo"><img src="{{ asset('images/logo.png')}}" width="190" height="60"  alt=""/></div>
+    <div class="uinfo fn-clear">
+      <div class="uface"><img src="{{ asset('images/hetu.jpg')}}" width="40" height="40"  alt=""/></div>
+      <div class="uname">
+        河图<i class="fontico down"></i>
+        <ul class="managerbox">
+          <li><a href="#"><i class="fontico lock"></i>修改密码</a></li>
+          <li><a href="#"><i class="fontico logout"></i>退出登录</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="chat_message fn-clear">
+    <div class="chat_left">
+      <div class="message_box" id="message_box">
 
-    function song(){
-        var text = document.getElementById('text').value;
-        document.getElementById('text').value = '';
-        //向服务器发送数据
-        websocket.send(text);
-    }
-    //监听连接关闭
-    //    websocket.onclose = function (evt) {
-    //        console.log("Disconnected");
-    //    };
-
-    //onmessage 监听服务器数据推送
-    websocket.onmessage = function (evt) {
-        msg.innerHTML += evt.data +'<br>';
-        console.log('Retrieved data from server: ' + evt);
-    };
-    //监听连接错误信息
-    //    websocket.onerror = function (evt, e) {
-    //        console.log('Error occured: ' + evt.data);
-    //    };
-
+        {{--<div class="msg_item fn-clear">
+          <div class="uface"><img src="{{ asset('images/hetu.jpg')}}" width="40" height="40"  alt=""/></div>
+          <div class="item_right">
+            <div class="msg own">那个统计表也不能说明一切</div>
+            <div class="name_time">河图 · 30秒前</div>
+          </div>
+        </div>--}}
+      </div>
+      <div class="write_box">
+        <textarea id="message" name="message" class="write_area" placeholder="说点啥吧..."></textarea>
+        <input type="hidden" name="fromname" id="fromname" value="河图" />
+        <input type="hidden" name="to_uid" id="to_uid" value="0">
+        <div class="facebox fn-clear">
+          <div class="expression"></div>
+          <div class="chat_type" id="chat_type">群聊</div>
+          <button name="" class="sub_but">提 交</button>
+        </div>
+      </div>
+    </div>
+    <div class="chat_right">
+      <ul class="user_list" title="双击用户私聊">
+        <li class="fn-clear selected"><em>所有用户</em></li>
+        <li class="fn-clear" data-id="1"><span><img src="{{ asset('images/hetu.jpg')}}" width="30" height="30"  alt=""/></span><em>河图</em><small class="online" title="在线"></small></li>
+        <li class="fn-clear" data-id="2"><span><img src="{{ asset('images/53f44283a4347.jpg')}}" width="30" height="30"  alt=""/></span><em>猫猫</em><small class="online" title="在线"></small></li>
+        <li class="fn-clear" data-id="3"><span><img src="{{ asset('images/53f442834079a.jpg')}}" width="30" height="30"  alt=""/></span><em>白猫</em><small class="offline" title="离线"></small></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+$(document).ready(function(e) {
+	$('#message_box').scrollTop($("#message_box")[0].scrollHeight + 20);
+	$('.uname').hover(
+	    function(){
+		    $('.managerbox').stop(true, true).slideDown(100);
+	    },
+		function(){
+		    $('.managerbox').stop(true, true).slideUp(100);
+		}
+	);
+	
+	var fromname = $('#fromname').val();
+	var to_uid   = 0; // 默认为0,表示发送给所有用户
+	var to_uname = '';
+	$('.user_list > li').dblclick(function(){
+		to_uname = $(this).find('em').text();
+		to_uid   = $(this).attr('data-id');
+		if(to_uname == fromname){
+		    alert('您不能和自己聊天!');
+			return false;
+		}
+		if(to_uname == '所有用户'){
+		    $("#toname").val('');
+			$('#chat_type').text('群聊');
+		}else{
+		    $("#toname").val(to_uid);
+			$('#chat_type').text('您正和 ' + to_uname + ' 聊天');
+		}
+		$(this).addClass('selected').siblings().removeClass('selected');
+	    $('#message').focus().attr("placeholder", "您对"+to_uname+"说：");
+	});
+	
+	$('.sub_but').click(function(event){
+	    sendMessage(event, fromname, to_uid, to_uname);
+	});
+	
+	/*按下按钮或键盘按键*/
+	$("#message").keydown(function(event){
+		var e = window.event || event;
+        var k = e.keyCode || e.which || e.charCode;
+		//按下ctrl+enter发送消息
+		if((event.ctrlKey && (k == 13 || k == 10) )){
+			sendMessage(event, fromname, to_uid, to_uname);
+		}
+	});
+});
+function sendMessage(event, from_name, to_uid, to_uname){
+    var msg = $("#message").val();
+	if(to_uname != ''){
+	    msg = '您对 ' + to_uname + ' 说： ' + msg;
+	}
+	var htmlData =   '<div class="msg_item fn-clear">'
+                   + '   <div class="uface"><img src="{{ asset('images/hetu.jpg')}}" width="40" height="40"  alt=""/></div>'
+			       + '   <div class="item_right">'
+			       + '     <div class="msg own">' + msg + '</div>'
+			       + '     <div class="name_time">' + from_name + ' · 30秒前</div>'
+			       + '   </div>'
+			       + '</div>';
+	$("#message_box").append(htmlData);
+	$('#message_box').scrollTop($("#message_box")[0].scrollHeight + 20);
+	$("#message").val('');
+}
 </script>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,50 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div id="msg"></div>
+<input type="text" id="text">
+<input type="submit" value="发送数据" onclick="song()">
+</body>
+<script>
+    var msg = document.getElementById("msg");
+    var wsServer = 'ws://106.14.10.215:9505';
+    var websocket = new WebSocket(wsServer);
+    //onopen监听连接打开
+    websocket.onopen = function (evt) {
+        //websocket.readyState 属性：
+        /*
+         CONNECTING    0    The connection is not yet open.
+         OPEN    1    The connection is open and ready to communicate.
+         CLOSING    2    The connection is in the process of closing.
+         CLOSED    3    The connection is closed or couldn't be opened.
+         */
+        msg.innerHTML = websocket.readyState;
+    };
 
-        <title>Laravel</title>
+    function song(){
+        var text = document.getElementById('text').value;
+        document.getElementById('text').value = '';
+        //向服务器发送数据
+        websocket.send(text);
+    }
+    //监听连接关闭
+    //    websocket.onclose = function (evt) {
+    //        console.log("Disconnected");
+    //    };
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    //onmessage 监听服务器数据推送
+    websocket.onmessage = function (evt) {
+        msg.innerHTML += evt.data +'<br>';
+        console.log('Retrieved data from server: ' + evt);
+    };
+    //监听连接错误信息
+    //    websocket.onerror = function (evt, e) {
+    //        console.log('Error occured: ' + evt.data);
+    //    };
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+</script>
 </html>
