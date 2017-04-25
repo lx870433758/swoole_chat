b073e25f750a4d7809f2aba39360d9fe51c12176
lx870433758 <870433758@qq.com>
2017-04-25 22:57:30 +0800

    no message

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,6 +8,7 @@ class ClientController extends Controller
 {
     //
     public function index(Request $request){
+        session('user', $request->user());
         return view('Client.index',['request' => $request]);
     }
 }
