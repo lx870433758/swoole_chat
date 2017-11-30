757bd005f9b7a5a7f406844f2aa6a567fe0ef5fa
lx <870433758@qq.com>
2017-11-30 16:38:07 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
 });
 Route::group(['middleware' => 'auth.user'], function () {
     Route::get('/client/index','ClientController@index');
-    Route::get('/client/index','ClientController@index');
     Route::post('/client/user_bind','ClientController@user_bind');
     Route::post('/client/get_user_info','ClientController@get_user_info');
 });
