01077129acbca17cbc14daa184f1964b0d217235
lx <870433758@qq.com>
2017-11-30 16:36:39 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::group(['middleware' => 'auth.user'], function () {
     Route::get('/client/index','ClientController@index');
-    Route::get('/','ClientController@index');
     Route::post('/client/user_bind','ClientController@user_bind');
     Route::post('/client/get_user_info','ClientController@get_user_info');
 });
