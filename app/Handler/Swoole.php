75adf5e2671cb41e156accc549f7e21531b8add1
lx <870433758@qq.com>
2017-12-01 10:12:28 +0800

    no message

--- app/Handler/Swoole.php
+++ app/Handler/Swoole.php
@@ -32,7 +32,8 @@ class Swoole extends \swoole_websocket_server{
 
             //更新用户列表
             $user_list = $redis->exists('user_list') ? json_decode($redis->get('user_list'),true): [];
-            $checkAdd = isset($user_list[(string)$id]) ? 1:0;
+            $checkAdd = isset($user_list[$id]) ? 1:0;
+            echo json_encode($user_list[$id]);
             $user_list[$id] = $userInfo;
             $redis->set('user_list', json_encode($user_list)) ;
 
